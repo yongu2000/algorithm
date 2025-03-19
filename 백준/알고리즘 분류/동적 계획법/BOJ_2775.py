166c0f5c667c99a8a918b737df3024803d476127
YongU Cho <yongu2000@naver.com>
2025-03-20 00:10:17 +0900

    2025-03-20

---
+++ 백준/알고리즘 분류/동적 계획법/BOJ_2775.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+
+    dp = [[0]*(n+1) for _ in range(k+1)]
+
+    for i in range(1, n+1):
+        dp[0][i] = i
+
+    for i in range(1, k+1):
+        for j in range(1, n+1):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            
+    print(dp[k][n])
+    
