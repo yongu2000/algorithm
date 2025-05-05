9bae14aa6f9c4686c2ae736249710554e6e49788
YongU Cho <yongu2000@naver.com>
2025-05-06 01:31:00 +0900

    2025-05-06

--- 백준/문제집/IT기업 및 대기업 계열사 코테보면서 비슷했던 문제들/BOJ_4179.py
+++ 백준/문제집/IT기업 및 대기업 계열사 코테보면서 비슷했던 문제들/BOJ_4179.py
@@ -1,14 +1,46 @@
 import sys
+from collections import deque
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+def bfs(start, fire_start):
+    q = deque()
+    for f in fire_start:
+        f_sx, f_sy = f
+        q.append((f_sx, f_sy, 1, "F"))
+    s_x, s_y = start
+    q.append((s_x, s_y, 1, "J"))
+
+    while q:
+        x, y, dist, obj = q.popleft()
+
+        if obj == "J" and (x == 0 or y == 0 or x == r-1 or y == c-1):
+            return dist
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < r and 0 <= ny < c and maze[nx][ny] == ".":
+                maze[nx][ny] = obj
+                q.append((nx, ny, dist+1, obj))
+        
+    return "IMPOSSIBLE"
 
 input = sys.stdin.readline
 
-n = int(input())
-children = [int(input()) for _ in range(n)]
-dp = [1] * n
+r, c = map(int, input().split())
 
-for i in range(1, n):
-    for j in range(i):
-        if children[i] > children[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+maze = [list(input().strip()) for _ in range(r)]
 
-print(n - max(dp))
+start = ()
+fire_start = []
+for i in range(r):
+    for j in range(c):
+        if maze[i][j] == "J":
+            start = (i, j)
+        elif maze[i][j] == "F":
+            fire_start.append((i, j))
+
+print(bfs(start, fire_start))
